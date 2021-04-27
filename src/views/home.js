0f26bffe02db867925a04e55ce560b68da522dd1
Katherine-fe <kattyserranobasan@gmail.com>
2021-04-27 11:08:48 -0500

    post

--- src/views/home.js
+++ src/views/home.js
@@ -1,3 +1,5 @@
+import addPost, { editPost, removePost } from '../model/firebase-post-model.js';
+
 export default () => {
   const viewRegister = `<header id="main-header" class ="header">
   <div class="menu-bar">
@@ -16,9 +18,61 @@ export default () => {
     <span class="material-icons">menu</span>
   </div>
 </header>
-<!-- main header -->`;
+<!-- main header -->
+<textarea name="" id="input-new-note" rows="4" cols="50" placeholder="¿Que quieres compartir?"></textarea>
+<section id="botones-post">
+<button type="button" id="btn-add-note">Publicar</button>
+</section>
+<section>
+<div id="commentPublish">write here comment</div>
+</section>
+`;
   const reg = document.getElementById('main-login');
   reg.innerHTML = '';
   reg.innerHTML = viewRegister;
+  const notePost = document.getElementById('btn-add-note');
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log(notePost);
+    if (user) {
+      notePost.addEventListener('click', () => {
+        const post = document.getElementById('input-new-note').value;
+        addPost(post).then((docRef) => {
+          console.log(docRef.id);
+          document.getElementById('input-new-note').value = '';
+        }).catch((err) => {
+          console.error('Error adding document: ', err);
+        });
+      });
+      firebase.firestore().collection('post')
+        .orderBy('datePost', 'desc')
+        .onSnapshot((querySnapshot) => {
+          const commentPublish = document.getElementById('commentPublish');
+          commentPublish.innerHTML = '';
+          let i = 0;
+          querySnapshot.forEach((doc) => {
+            // eslint-disable-next-line no-plusplus
+            i++;
+            commentPublish.innerHTML += `<div class = "publishRS"><span>-----------comentario ${i} ${
+              doc.data().publication
+            }<button type="button" class="removeBtn-${
+              doc.id
+            }" >remove<button type="button" class="editBtn-${
+              doc.id
+            }">edit</span></div> `;
+          });
+          querySnapshot.forEach((doc) => {
+            if (firebase.auth().currentUser.uid === doc.data().uid) {
+              editPost(doc.id);
+              removePost(doc.id);
+            }
+          });
+        });
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('necesitas loguearte para hacer esta operación');
+      window.location.hash = '#/login';
+    }
+  });
+
   return reg;
 };
